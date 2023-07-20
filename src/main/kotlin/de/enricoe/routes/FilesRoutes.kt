55486ec201f6f7ea260ed5a878f4f401d1a38291
enricoxD <noreply@enricoe.de>
2023-07-20 04:42:12 +0200

    remove redundant authenticate(optional) block

--- src/main/kotlin/de/enricoe/routes/FilesRoutes.kt
+++ src/main/kotlin/de/enricoe/routes/FilesRoutes.kt
@@ -13,50 +13,54 @@ import java.io.File
 
 fun Routing.filesRoutes() {
     route("/file") {
-        authenticate(optional = true) {
-            post("upload") {
-                val userId = call.principal<UserIdPrincipal>()?.name ?: "unknown"
-                val result = FilesRepository.uploadFiles(userId, call.receiveMultipart())
+        post("upload") {
+            val userId = call.principal<UserIdPrincipal>()?.name ?: "unknown"
+            val result = FilesRepository.uploadFiles(userId, call.receiveMultipart())
+            call.respond(result.statusCode, result)
+        }
+
+        post("getupload") {
+            val userId = call.principal<UserIdPrincipal>()?.name
+            val request = call.receive<GetUploadRequest>()
+            val result = FilesRepository.getUpload(userId, request.author, request.id, request.password)
+            call.respond(result.statusCode, result)
+        }
+
+        post("requestdownload") {
+            val userId = call.principal<UserIdPrincipal>()?.name
+            val request = call.receive<FileDownloadRequest>()
+            val result = FilesRepository.requestFileDownload(
+                userId,
+                request.author,
+                request.id,
+                request.fileUpload,
+                request.password
+            )
+
+            if (result is Response.Error) {
                 call.respond(result.statusCode, result)
+            } else if (result is Response.Success) {
+                call.respondFile(result.data as File)
             }
+        }
 
-            post("getupload") {
-                val userId = call.principal<UserIdPrincipal>()?.name
-                val request = call.receive<GetUploadRequest>()
-                val result = FilesRepository.getUpload(userId, request.author, request.id, request.password)
+        post("requestdownloadall") {
+            val userId = call.principal<UserIdPrincipal>()?.name
+            val request = call.receive<GetUploadRequest>()
+            val result = FilesRepository.requestDownloadAll(userId, request.author, request.id, request.password)
+
+            if (result is Response.Error) {
                 call.respond(result.statusCode, result)
+            } else if (result is Response.Success) {
+                call.respondFile(result.data as File)
             }
+        }
 
-            post("requestdownload") {
-                val userId = call.principal<UserIdPrincipal>()?.name
-                val request = call.receive<FileDownloadRequest>()
-                val result = FilesRepository.requestFileDownload(userId, request.author, request.id, request.fileUpload, request.password)
-
-                if (result is Response.Error) {
-                    call.respond(result.statusCode, result)
-                } else if (result is Response.Success) {
-                    call.respondFile(result.data as File)
-                }
-            }
-
-            post("requestdownloadall") {
-                val userId = call.principal<UserIdPrincipal>()?.name
-                val request = call.receive<GetUploadRequest>()
-                val result = FilesRepository.requestDownloadAll(userId, request.author, request.id, request.password)
-
-                if (result is Response.Error) {
-                    call.respond(result.statusCode, result)
-                } else if (result is Response.Success) {
-                    call.respondFile(result.data as File)
-                }
-            }
-
-            post("authorinformation") {
-                val userId = call.principal<UserIdPrincipal>()?.name
-                val request = call.receive<GetUploadRequest>()
-                val result = FilesRepository.requestAuthorInformation(userId, request.author, request.id, request.password)
-                call.respond(result.statusCode, result)
-            }
+        post("authorinformation") {
+            val userId = call.principal<UserIdPrincipal>()?.name
+            val request = call.receive<GetUploadRequest>()
+            val result = FilesRepository.requestAuthorInformation(userId, request.author, request.id, request.password)
+            call.respond(result.statusCode, result)
         }
     }
 }
